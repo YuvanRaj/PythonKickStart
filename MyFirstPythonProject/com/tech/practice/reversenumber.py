6bf0537e8a0d2fd1834c4791cfcf9290ec7d6de6
YuvanRaj <yuviram91@gmail.com>
2020-01-05 22:34:45 +0530

    Reverse a Number in Python

---
+++ MyFirstPythonProject/com/tech/practice/reversenumber.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 5, 2020
+
+@author: Yuvaraj
+'''
+
+num=259
+print(num)
+reveed=0
+digit=None
+while num != 0:
+    digit = num%10
+    reveed = reveed*10+digit
+    num=num//10
+
+print(reveed)    
+    
